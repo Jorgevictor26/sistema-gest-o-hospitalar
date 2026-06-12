afe79c4772937986d4a73db04be5ee90309037b7
Jorge Victor <jorgekvictor26@gmail.com>
2026-06-12 08:27:06 +0100

    Migratiosn updates

--- backend/database/migrations/2026_06_11_164121_create_patients_table.php
+++ backend/database/migrations/2026_06_11_164121_create_patients_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('phone_number');
             $table->enum('gender', ['male', 'female']);
+            $table->date('date_of_birth')->nullable();
             $table->string('identity_card')->unique;
             $table->string('email')->unique->nullable();
             $table->string('address')->nullable();
