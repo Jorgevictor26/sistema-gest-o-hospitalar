07919408ca5933cdd4c81b54ca7152866f97fe83
Jorge Victor <jorgekvictor26@gmail.com>
2026-06-18 14:15:32 +0100

    RegisterDTO

---
+++ backend/app/DTOs/RegisterDTO.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DTOs;
+
+
+class RegisterDTO
+{
+
+    public function  __construct(
+        public readonly string $name,
+        public readonly string $email,
+        public readonly string $phone_number,
+        public readonly string $password
+    ) {}
+
+    public function fromArray(array $data): self {
+        return new self(
+            name: $data['name'],
+            email: $data['email'],
+            phone_number: $data['phone_number'],
+            password: $data['password']
+        );
+    }
+}
