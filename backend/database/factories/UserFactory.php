03f80049bf2078aad9a2cd946ce5f49f7e4a29fa
Jorge Victor <jorgekvictor26@gmail.com>
2026-06-18 12:42:12 +0100

    DatabaseSeeder and factory

---
+++ backend/database/factories/UserFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+
+/**
+ * @extends Factory<User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = User::class;
+    
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'phone_number' => fake()->phoneNumber(),
+            'password' => Hash::make('password'), // padrão para testes
+            'role' => fake()->randomElement(['admin', 'receptionist', 'doctor']),
+            'is_active' => true,
+            'remember_token' => Str::random(10),
+        ];
+    }
+}
